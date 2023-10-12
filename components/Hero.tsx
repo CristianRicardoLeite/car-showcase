54b2e62d445755ca7aabcbe1f6b807c5f43c31ef
CristianRicardoLeite <cristianricardoleite@gmail.com>
2023-10-12 14:28:26 -0300

    add footer and navbar components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,16 +15,26 @@ const Hero = () => {
                 </h1>
 
 
-            </div>
-            <p className='hero__subtitle'>
-                Streamline your car rental experience with our effortless booking process.
-            </p>
 
-            <CustomButton
-                title="Explore Cars"
-                containerStyles="bg-primary-blue text-shite rounded-full mt-10"
-                handleClick={handleScroll}
-            />
+                <p className='hero__subtitle'>
+                    Streamline your car rental experience with our effortless booking process.
+                </p>
+                <CustomButton
+                    title="Explore Cars"
+                    containerStyles="bg-primary-blue text-shite rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
+            </div>
+            <div className='hero__image-container'>
+                <div className='hero__image'>
+                    <Image
+                        src='/hero.png'
+                        alt='hero'
+                        fill className='object-contain'
+                    />
+                    <div className='hero__image-overlay' />
+                </div>
+            </div>
         </div>
     )
 }
